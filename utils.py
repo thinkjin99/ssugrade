706b7b29adb85b9d31a8caa08233077fe6783a7a
Jin <thinkjin99@gmail.com>
2022-12-26 12:14:23 +0900

    Refator code structure
    코드 구조를 전반적으로 변경했습니다. 함수 -> 클래스 사용
    로그인, 유틸 등의 모듈을 추가적으로 생성해 scrapper.py에서는 스크래퍼와 관련된 부분만
    전담하는 형태로 변경했습니다.

--- utils.py
+++ utils.py
@@ -1,7 +1,20 @@
-from constant import *
+import time
+async def max_retry(function, **kwargs):
+    for _ in range(3):
+        try:
+            res = await function(**kwargs) if kwargs else await function()
+            if res: break
+        
+        except TimeoutError: continue
 
-def get_payload(student_id:str, password:str):
-    payload = PAYLOAD.copy()
-    payload['sap-user'] = student_id
-    payload['sap-password'] = password
-    return payload
+    if not res: raise AssertionError(f"{function} occurs error!")
+    return res
+
+
+def parse_grade(inner_texts:str):
+    column_names = ['이수학년도','이수학기','과목코드','과목명','과목학점','성적','등급','교수명','비고']
+    column_len = len(column_names)
+    table_texts = inner_texts.split('\t')
+    text_by_rows = [table_texts[i:i + len(column_names)] for i in range(column_len + 2, len(table_texts), column_len + 1)]
+    grade_info = [{col:value for col, value in zip(column_names,row)} for row in text_by_rows]
+    return grade_info
