fe2ba80d61c95e5e61f552b587035d189634c3c2
Jin <thinkjin99@gmail.com>
2023-08-29 21:00:44 +0900

    Add login to get login cookie

---
+++ login.py
@@ -0,0 +1,27 @@
+from constant import *
+import requests
+
+
+def get_payload(student_id: str, password: str):
+    payload = PAYLOAD.copy()
+    payload["sap-user"] = student_id
+    payload["sap-password"] = password
+    return payload
+
+
+async def get_login_cookie(student_id: str, password: str) -> list[dict[str, str]]:
+    payload = get_payload(student_id, password)
+    login_res = requests.post(URL, data=payload, allow_redirects=False, timeout=3)
+    login_cookies = login_res.cookies
+    if "MYSAPSSO2" not in login_cookies.keys():
+        raise AssertionError("Wrong ID or Password")  # 401에러 등의 응답 메시지 줘야 할듯
+    cookie_list = [
+        {
+            "name": cookie.name,
+            "value": cookie.value,
+            "domain": cookie.domain,
+            "path": cookie.path,
+        }
+        for cookie in login_cookies
+    ]
+    return cookie_list
