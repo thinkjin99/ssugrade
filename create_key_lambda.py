33093dbc5b9befd7dafd8540121ad04c0e579ba5
정명진 <thinkjin99@gmail.com>
2023-11-13 16:09:33 +0900

    Add RSA key issue lambda
    RSA 키를 생성해 발급하는 람다를 작성했습니다.
    해당 람다는 로그인 검증후 키를 발급하고 DB에 FCM 토큰과 RSA 키를 저장합니다.
    키와 토큰은 upsert로 관리됩니다.

---
+++ create_key_lambda.py
@@ -0,0 +1,40 @@
+import json
+import sqlalchemy
+from Crypto.PublicKey import RSA
+
+from database import execute_query
+from refresh_cookie_lambda import login
+from lambda_utils import lamdba_decorator
+
+
+def create_key():
+    key = RSA.generate(1024)
+    private_key = key.exportKey()
+    public_key = key.publickey().exportKey()
+    return private_key, public_key
+
+
+@execute_query
+def save_public_key(student_number: str, public_key: str, fcm_token: str):
+    public_key = "\n".join(public_key.split("\n")[1:-1])
+    stmt = f"""
+    INSERT INTO users(student_number, public_key, fcm_token) VALUES("{student_number}","{public_key}","{fcm_token}") 
+    ON DUPLICATE KEY UPDATE 
+    public_key="{public_key}",
+    fcm_token="{fcm_token}"
+    """  # 정보 upsert
+    query = sqlalchemy.text(stmt)
+    return query
+
+
+@lamdba_decorator
+def handler(event, context) -> str:
+    body = json.loads(event["body"])
+    student_number = body["student_number"]
+    password = body["password"]
+    fcm_token = body["fcm_token"]
+
+    login(student_number, password)
+    private_key, public_key = create_key()
+    save_public_key(student_number, public_key.decode(), fcm_token)
+    return private_key.decode()
