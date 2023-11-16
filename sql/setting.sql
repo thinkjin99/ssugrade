3969688681a078b2747823f9c275bd61eaabe47c
정명진 <thinkjin99@gmail.com>
2023-11-16 22:34:38 +0900

    Change folder name to lambda

---
+++ sql/setting.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `ssugrade` USE `ssugrade`;
+
+CREATE TABLE
+    IF NOT EXISTS `users` (
+        `student_number` VARCHAR(8) PRIMARY KEY,
+        `fcm_token` VARCHAR(255) NOT NULL,
+        `public_key` VARCHAR(128) NOT NULL,
+        `grade` VARCHAR(128)
+    );
