ca75a7c24e1a1f96b6d8e8b5831504ff3bc733f6
정명진 <thinkjin99@gmail.com>
2023-11-22 17:03:07 +0900

    Add cookie json save in db
    로그인 쿠키 정보를 DB에 json 방식으로 저장하는 코드를 추가했습니다.
    이제 초기 로그인시 기본적으로 json 컬럼에 쿠키가 저장됩니다.

--- lambda/codes/create_key_lambda.py
+++ lambda/codes/create_key_lambda.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from Crypto.PublicKey import RSA
 
 from database import execute_query
-from refresh_cookie_lambda import login
+from refresh_cookie_lambda import login, get_cookie
 from lambda_utils import lamdba_decorator
 
 
@@ -15,27 +15,30 @@ def create_rsa_key():
 
 
 @execute_query
-def save_public_key(student_number: str, public_key: str, fcm_token: str):
+def insert_user_data(
+    student_number: str, public_key: str, fcm_token: str, cookies: list
+):
     public_key = "\n".join(public_key.split("\n")[1:-1])
     stmt = f"""
-    INSERT INTO users(student_number, public_key, fcm_token) VALUES("{student_number}","{public_key}","{fcm_token}") 
+    INSERT INTO users(student_number, public_key, fcm_token, cookies) VALUES("{student_number}","{public_key}","{fcm_token}",'{json.dumps(cookies)}') 
     ON DUPLICATE KEY UPDATE 
     public_key="{public_key}",
-    fcm_token="{fcm_token}"
+    fcm_token="{fcm_token}",
+    cookies = '{json.dumps(cookies)}'
     """  # 정보 upsert
     query = sqlalchemy.text(stmt)
     return query
 
 
 @lamdba_decorator
-def handler(event, context) -> str:
+def handler(event, context) -> dict:
     body = json.loads(event["body"])
     student_number = body["student_number"]
     password = body["password"]
     fcm_token = body["fcm_token"]
 
-    login(student_number, password)
+    response = login(student_number, password)
+    cookies = get_cookie(response)
     private_key, public_key = create_rsa_key()
-    save_public_key(student_number, public_key.decode(), fcm_token)
-    return private_key.decode()
-
+    insert_user_data(student_number, public_key.decode(), fcm_token, cookies)
+    return {"private_key": private_key.decode()}
--- sql/setting.sql
+++ sql/setting.sql
@@ -5,5 +5,6 @@ CREATE TABLE
         `student_number` VARCHAR(8) PRIMARY KEY,
         `fcm_token` VARCHAR(255) NOT NULL,
         `public_key` VARCHAR(128) NOT NULL,
-        `grade` VARCHAR(128)
+        `grade` VARCHAR(128),
+        `cookies` JSON
     );
