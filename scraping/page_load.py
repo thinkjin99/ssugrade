eda53d50351cc7ace77263a7e6104933c0792d84
정명진 <thinkjin99@gmail.com>
2023-11-24 12:59:14 +0900

    Add cookie valid checker

--- scraping/page_load.py
+++ scraping/page_load.py
@@ -2,15 +2,12 @@ from playwright.async_api import async_playwright, BrowserContext, Browser, Page
 from constant import *
 
 
-
-
 async def create_default_browser():
     playwright = await async_playwright().start()
     browser = await playwright.chromium.launch(
         args=[
             "--headless",  # 백그라운드에서 실행
             "--disable-gpu",  # GPU 활용 하지 않음
-            "--single-process",  # 싱글 프로세스로 실행
             "--no-sandbox",  # GPU 사용안함
             "--disable-extensions",
             "--incognito",
@@ -24,11 +21,17 @@ async def create_default_browser():
     return browser  # 함수의 정상종료를 나타낸다.
 
 
+async def check_cookie_valid(page: Page):
+    title = await page.locator("title").text_content()
+    if title == "로그온":  # 쿠키 유효여부 확인
+        raise AssertionError("Login Error!")
+
+
 async def load_usaint_page(browser: Browser, cookie_list: list[dict]) -> Page:
     context = await browser.new_context()  # 브라우저 생성
     await context.add_cookies(cookie_list)  # 매개변수가 SetCookieParam 형식이여야 한다.
     page = await load_page(context)
-    # await click_popup(page, ".urPWFloatRight")  # 팝업을 닫습니다./
+    await check_cookie_valid(page)
     return page
 
 
