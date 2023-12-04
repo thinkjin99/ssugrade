6eaafc3dbb7d1af5b31d9ca053c568fec1ac6815
정명진 <thinkjin99@gmail.com>
2023-12-04 19:49:53 +0900

    Add fastapi handler

--- scraping/code/main.py
+++ scraping/code/main.py
@@ -3,7 +3,10 @@ from pydantic import BaseModel
 
 from fastapi.responses import JSONResponse
 
-from scrap import run_single_browser_scrap_now, run_single_browser_scrap_all
+from scrap import (
+    run_single_browser_scrap_now,
+    run_multy_browser_scrapl_all_grades,
+)
 
 app = FastAPI()
 
@@ -15,7 +18,7 @@ class User(BaseModel):
 @app.post("/grade/all")
 async def _scrap_all(user: User):
     try:
-        data = await run_single_browser_scrap_all(user.student_id)
+        data = await run_multy_browser_scrapl_all_grades(user.student_id)
         return JSONResponse(content=data, status_code=200)
 
     except Exception as e:
