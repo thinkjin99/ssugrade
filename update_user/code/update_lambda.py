9e73aa6ed735293a75bb1d79e99f78663dc8acdb
정명진 <thinkjin99@gmail.com>
2023-12-19 14:46:59 +0900

    Fix update query to upsert to insert first init

--- update_user/code/update_lambda.py
+++ update_user/code/update_lambda.py
@@ -1,7 +1,7 @@
 import json
 import sqlalchemy
 
-from lambda_utils import *
+from update_user.code.lambda_utils import *
 from constant import *
 from database import execute_query
 
@@ -9,7 +9,11 @@ from database import execute_query
 @execute_query
 def update_cookie(student_number: str, cookies: list, fcm_token: str | None = None):
     if fcm_token:
-        stmt = f'''UPDATE users SET cookies='{json.dumps(cookies)}', fcm_token="{fcm_token}" WHERE student_number="{student_number}"'''
+        stmt = f"""
+                INSERT INTO users(student_number, fcm_token, cookies) 
+                VALUES("{student_number}","{fcm_token}",'{json.dumps(cookies)}')
+                ON DUPLICATE KEY UPDATE fcm_token="{fcm_token}",cookies = '{json.dumps(cookies)}'
+                """
     else:
         stmt = f'''UPDATE users SET cookies='{json.dumps(cookies)}' WHERE student_number="{student_number}"'''
     query = sqlalchemy.text(stmt)
